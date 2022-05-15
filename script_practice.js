4e807918927b12e4b04714599ccb49376f0447de
zhora_95 <sh_nastia95@mail.ru>
2022-05-15 22:26:25 +0300

    Practice_part2

--- script_practice.js
+++ script_practice.js
@@ -6,11 +6,19 @@ let personalMovieDB = {
     genres: {},
     privat: false
 };
-const firstlastMovie = prompt("Один из последних просмотренных фильмов?"),
-    firstLastPoint = prompt("На сколько оцените фильм?"),
-    secondlastMovie = prompt("Один из последних просмотренных фильмов?"),
-    secondLastPoint = prompt("На сколько оцените фильм?")
-    ;
-personalMovieDB.movies[firstlastMovie] = firstLastPoint;
-personalMovieDB.movies[secondlastMovie] = secondLastPoint;
-console.log(personalMovieDB);
+let lastMovie, lastPoint;
+for (let i = 0; i <= 1;) {
+    lastMovie = prompt("Один из последних просмотренных фильмов?");
+    lastPoint = prompt("На сколько оцените фильм?");
+    if (lastMovie === null || lastPoint === null || lastMovie === "" || lastPoint === "" || lastMovie.length > 50 || lastMovie.length == 0)
+        continue;
+    personalMovieDB.movies[lastMovie] = lastPoint;
+    i++;
+}
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено доволи мало фильмов...');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель...');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман...');
+};

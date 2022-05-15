b9ff5349e8cd49c0018bb81bd734ffa563cad7a4
zhora_95 <sh_nastia95@mail.ru>
2022-05-15 12:37:53 +0300

    loops

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ if (hamburger === 3 && cola || fries === 3 && nuggets) {
 }
 console.log(hamburger === 3 && cola || fries === 3 && nuggets); // true && cola = cola -> true && nuggets = nuggets -> cola || nuggets = nuggets
 
-
 let hamburger;
 const fries = NaN;
 const cola = 0;
@@ -53,7 +52,81 @@ if (hamburger && cola || fries === 3 && nuggets) {
 	console.log('Done!')
 }
 console.log(hamburger && cola || fries === 3 && nuggets); // hamburger && cola = hamburger -> fries === 3 && nuggets = false -> hamburger || false = false
+*/ //Loop tasks 
+/*
+// Место для первой задачи
+function firstTask() {
+	let i = 5;
+	while (i <= 10) {
+		console.log(i);
+		i++;
+	}
+}
+
+// Место для второй задачи
+function secondTask() {
+	for (let i = 20; i >= 10; i--) {
+		if (i === 13) {
+			break;
+		}
+		console.log(i);
+	}
+}
+
+// Место для третьей задачи
+function thirdTask() {
+	for (let i = 2; i <= 10;) {
+		console.log(i);
+		i += 2;
+	}
+}
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+	let i = 2;
+	while (i <= 16) {
+		console.log(i);
+		i += 2;
+	}
+}
+
+fourthTask();
+// Место для пятой задачи
+function fifthTask() {
+	const arrayOfNumbers = [];
+
+	for (let i = 5; i <= 10; i++) {
+		arrayOfNumbers.push(i);
+	}
+	// Не трогаем
+	return arrayOfNumbers;
+}
+
+const lines = 5;
+let result = '';
+for (let i = 0; i <= lines; i++) {
+	for (let j = 0; j < lines - i; j++) {
+		result += ' ';
+	}
+
+	for (let y = 0; y < i * 2 + 1; y++) {
+		result = result + '*';
+	}
+	result += '\n';
+}
+console.log(result);
 */
+
 // CALLBACK
 /*
 function first(){
